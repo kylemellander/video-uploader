d988d842f87d0e2567eeef333d445427a618e1ef
Kyle Mellander <kylemellander@gmail.com>
2016-03-24 20:16:59 -0700

    Fix typo

--- server/config/application.rb
+++ server/config/application.rb
@@ -23,6 +23,6 @@ module VideoUploadServer
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.api_only = trues
+    config.api_only = true
   end
 end
