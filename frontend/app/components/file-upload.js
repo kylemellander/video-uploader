60c087206b3f48004a7a8ececa039261e888ae5c
Kyle Mellander <kylemellander@gmail.com>
2016-03-24 19:22:10 -0700

    Add checkExtensions method to fileupload

--- frontend/app/components/file-upload.js
+++ frontend/app/components/file-upload.js
@@ -4,7 +4,24 @@ export default Ember.TextField.extend({
   type: 'file',
   accept: '.mp4',
   change(e) {
-    // Upload file here
+    if (!e.target && !e.target.files) { return; }
+    const file = e.target.files[0];
+
+    if (!this.checkExtension(file) && !this.checkFileHeader(file)) {
+      return this.set('error', 'That file is not an mp4 file.');
+    }
+
+    // Upload file if it gets past the initial checks.
+
     return e;
+  },
+
+  checkExtension(file) {
+    return file.type === "video/mp4";
+  },
+
+  checkFileHeader(/*file*/) {
+    // Add library to check if mp4 file;
+    return true;
   }
 });
---
+++ frontend/tests/unit/components/file-upload-test.js
@@ -0,0 +1,19 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('component:file-upload', 'Unit | file upload', {
+  unit: true
+});
+
+test('should check if file extension is mp4', function(assert) {
+  assert.expect(2);
+
+  const component = this.subject();
+  const file = {name: "video.mp4", type: "video/mp4"};
+
+  assert.ok(component.checkExtension(file), 'file extension is mp4');
+
+  file.type = "image/jpg";
+  file.name = "image.jpg";
+
+  assert.notOk(component.checkExtension(file), 'file extension is not mp4');
+});
