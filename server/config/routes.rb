547baff6987acd47a58cfeab454db70155eef008
Kyle Mellander <kylemellander@gmail.com>
2016-03-24 20:39:12 -0700

    Add route to upload

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  post 'upload' => 'upload#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
