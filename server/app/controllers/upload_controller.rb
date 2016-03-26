559a91f2cc04295912cb9608ab097eb91ed45054
Kyle Mellander <kylemellander@gmail.com>
2016-03-26 12:33:53 -0700

    Save uploaded videos to video directory

--- server/app/controllers/upload_controller.rb
+++ server/app/controllers/upload_controller.rb
@@ -1,7 +1,36 @@
 class UploadController < ApplicationController
 
   def create
-    # handle json request here
+    filename = upload_params.original_filename
+
+    filename = increment_filename(filename)
+    File.open(video_dir + filename, 'w:ASCII-8BIT') { |file|
+      file.write(upload_params.read)
+    }
+    render json: {}
+  end
+
+  def increment_filename(filename)
+    count = 0
+    unique_name = filename
+    while File.exist?(video_dir + unique_name) do
+      count += 1
+      ext = File.extname(filename)
+      basename = File.basename(filename, ext)
+      unique_name = "#{basename}_#{count}#{ext}"
+    end
+
+    unique_name
+  end
+
+  private
+
+  def video_dir
+    'public/videos/'
+  end
+
+  def upload_params
+    params.require("0")
   end
 
 end
