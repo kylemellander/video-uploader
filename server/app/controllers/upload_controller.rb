a01c42420feaab61bb228b5b4bf333c9a5759087
Kyle Mellander <kylemellander@gmail.com>
2016-03-29 22:13:40 -0700

    Add checks to mp4 file before saving

--- server/app/controllers/upload_controller.rb
+++ server/app/controllers/upload_controller.rb
@@ -1,15 +1,16 @@
 class UploadController < ApplicationController
 
   def create
-    if upload_params && upload_params.class.to_s.split("::").last == "UploadedFile"
-      filename = upload_params.original_filename
-      filename = increment_filename(filename)
-      port = request.port == 80 ? "" : ":#{request.port}"
-      url = "#{request.protocol}#{request.host}#{port}/videos/#{filename}"
-
-      File.open("#{Rails.root}/#{video_dir}#{filename}", 'wb') { |file|
-        file.write(upload_params.read)
-      }
+    upload = upload_params["0"]
+    size = upload_params["size"]
+    if !check_is_file(upload)
+      render json: json_error("No File Uploaded", "There was no file attached to upload."), status: 422
+    elsif check_size(size, upload)
+      render json: json_error("File Too Large", "Maximum file size is 100MB."), status: 422
+    elsif !check_ext(upload.headers) || !check_mp4(upload.tempfile)
+      render json: json_error("Bad File", "That does not seem to be a valid mp4 file. Pick another video."), status: 422
+    else
+      url = write_file(upload)
 
       render json: {
         data: {
@@ -17,16 +18,6 @@ class UploadController < ApplicationController
             url: url
           }
         }
-      }
-    else
-      render json: {
-        "errors": [
-          {
-            "status": "422",
-            "title":  "No Attached File",
-            "detail": "There was no video file attached to the request."
-          }
-        ]
       }
     end
   end
@@ -54,7 +45,54 @@ class UploadController < ApplicationController
     unique_name
   end
 
-  def upload_params
-    params["0"]
+  def check_is_file(upload)
+    upload && upload.class.to_s.split("::").last == "UploadedFile"
   end
+
+  def check_size(size, upload)
+    size.to_i > 104857600 || upload.size() > 104857600
+  end
+
+  def check_ext(headers)
+    headers.include? "Content-Type: video/mp4"
+  end
+
+  def check_mp4(file)
+    begin
+      MP4Info.new(file)
+      true
+    rescue
+      file.rewind
+      false
+    end
+  end
+
+  def write_file(upload)
+    filename = upload.original_filename
+    filename = increment_filename(filename)
+    port = request.port == 80 ? "" : ":#{request.port}"
+    File.open("#{Rails.root}/#{video_dir}#{filename}", 'wb') { |file|
+      upload.rewind
+      file.write(upload.read)
+    }
+
+    "#{request.protocol}#{request.host}#{port}/videos/#{filename}"
+  end
+
+  def upload_params
+    params.permit("0", "size")
+  end
+
+  def json_error(title, detail)
+    {
+      "errors": [
+        {
+          "status": "422",
+          "title":  title,
+          "detail": detail
+        }
+      ]
+    }
+  end
+
 end
